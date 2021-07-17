69b4c9c9d8ee6f309f4014a1a3770c7aca8d7ec9
vitodigrigoli <73437231+vitodigrigoli@users.noreply.github.com>
2021-07-17 12:03:01 +0200

    add smooth scroll

--- js/vito.js
+++ js/vito.js
@@ -1,6 +1,7 @@
 const scroll_animation = document.getElementsByClassName("scroll")[0]
 let timer = null;
 
+//makes the animation visible when the user does not scroll for at least 3 seconds
 window.addEventListener('scroll', function() {
 	scroll_animation.classList.remove("scroll--visible")
 
@@ -13,3 +14,16 @@ window.addEventListener('scroll', function() {
 	}, 3000);
 
 }, false);
+
+//add smooth scrolling when clicking any anchor link
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+	anchor.addEventListener('click', function (e) {
+		e.preventDefault();
+		setTimeout( (e) => {
+			document.querySelector(this.getAttribute('href')).scrollIntoView({
+				behavior: 'smooth'
+			});
+		}, 400)
+
+	});
+});
