d321161f5eb94e3b5c7651f183f66b2d8dcdecce
vitodigrigoli <73437231+vitodigrigoli@users.noreply.github.com>
2021-07-19 12:20:26 +0200

    toggle menu animation

--- js/vito.js
+++ js/vito.js
@@ -1,17 +1,36 @@
 const scroll_animation = document.getElementsByClassName("scroll")[0]
+const logo = document.getElementsByClassName("logo")[0]
+const navbar = document.getElementsByClassName('navbar')[0]
 let timer = null;
+let last_scroll_top = 0;
 
 //makes the animation visible when the user does not scroll for at least 3 seconds
 window.addEventListener('scroll', function() {
 	scroll_animation.classList.remove("scroll--visible")
 
+	// scroll micro-interaction
 	if(timer !== null) {
 		clearTimeout(timer);
+
 	}
 
 	timer = setTimeout(function() {
 		scroll_animation.classList.add("scroll--visible")
 	}, 3000);
+
+
+ // display logo at upscroll
+	let st = window.pageYOffset || document.documentElement.scrollTop;
+	if (st > last_scroll_top){
+		// downscroll code
+		console.log("Scroll down")
+		logo.classList.add("logo--hidden")
+	} else {
+		// upscroll code
+		console.log("Scroll up")
+		logo.classList.remove("logo--hidden")
+	}
+	last_scroll_top = st <= 0 ? 0 : st; // For Mobile or negative scrolling
 
 }, false);
 
@@ -28,9 +47,9 @@ document.querySelectorAll('.bubbly-button').forEach(anchor => {
 	});
 });
 
-const logo = document.getElementsByClassName("logo")[0]
-const navbar = document.getElementsByClassName('navbar')[0]
+
 
 logo.addEventListener('click', () => {
 	navbar.classList.toggle('navbar--visible')
+	logo.classList.toggle('logo--animate')
 })
